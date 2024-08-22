f1e5328b240a0f737df2f2fff2d34e467dcff314
mash017 <mashrafiabrar052@gmail.com>
2024-08-22 13:08:33 +0600

    Update V. Comparison.cpp

--- V. Comparison.cpp
+++ V. Comparison.cpp
@@ -1,1 +1,43 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,c;
+    char b;
+    cin>>a>>b>>c;
+    if(b=='<')
+    {
+        if(a<c)
+        {
+            cout<<"Right"<<endl;
+        }
+        else
+        {
+            cout<<"Wrong"<<endl;
+        }
+    }
+   else if(b=='>')
+    {
+        if(a>c)
+        {
+            cout<<"Right"<<endl;
+        }
+        else
+        {
+            cout<<"Wrong"<<endl;
+        }
+    }
+   else if(b=='=')
+    {
+        if(a==c)
+        {
+            cout<<"Right"<<endl;
+        }
+        else
+        {
+            cout<<"Wrong"<<endl;
+        }
+    }
+ return 0;
+}
 
