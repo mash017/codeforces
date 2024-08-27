cb526c54582b7dc3b3f0efd135ade4acc0c3ebe6
mash017 <mashrafiabrar052@gmail.com>
2024-08-27 14:42:16 +0600

    Create Z. Hard Compare.c

---
+++ Z. Hard Compare.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ long long  int a,b,c,d;
+    scanf("%lld%lld%lld%lld",&a,&b,&c,&d);
+    if(b*log(a)>d*log(c))
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
