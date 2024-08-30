9b22becdbf866c069c152814b166c7661560875a
mash017 <mashrafiabrar052@gmail.com>
2024-08-30 23:10:22 +0600

    Create C. Next Alphabet.c

---
+++ C. Next Alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char x;
+    scanf("%c",&x);
+    if(x=='z')
+    {
+        printf("a\n");
+    }
+    else
+    {
+        printf("%c\n",x+1);
+    }
+    return 0;
+}
