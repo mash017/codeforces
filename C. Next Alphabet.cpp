7a9a3a66c5f5935cfb8a92af16ba2a73a3fbdf22
mash017 <mashrafiabrar052@gmail.com>
2024-08-30 23:11:10 +0600

    Create C. Next Alphabet.cpp

---
+++ C. Next Alphabet.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char x,b;
+    cin>>x;
+    b=x+1;
+    if(x=='z')
+    {
+        cout<<"a"<<endl;
+    }
+    else
+    {
+        cout<<b<<endl;
+    }
+    return 0;
+}
