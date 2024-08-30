10e6a5ffc35ed052d2329993e49585ea04e1f15b
mash017 <mashrafiabrar052@gmail.com>
2024-08-30 20:18:02 +0600

    Create A. Winter Sale.cpp

---
+++ A. Winter Sale.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double x,p,real;
+    cin>>x>>p;
+    real=(100*p)/(100-x);
+    cout<<fixed<<setprecision(2)<<real<<endl;
+    return 0;
+}
+
+
