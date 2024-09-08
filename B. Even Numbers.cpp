187727cc51f8daf704ee6299b572400b073be602
mash017 <mashrafiabrar052@gmail.com>
2024-09-08 12:15:00 +0600

    Create B. Even Numbers.cpp

---
+++ B. Even Numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+             cout<<i<<endl;
+        }
+       else if(n==1)
+       {
+           cout<<"-1"<<endl;
+       }
+    }
+    return 0;
+}
